d711f2773b01bf3a6d4d739e554537e5e7dcba5a
Shivanand Aabasaheb Karape <132795008+shivakarape@users.noreply.github.com>
2023-06-18 18:31:21 +0530

    Add files via upload

---
+++ program2.c
@@ -0,0 +1,15 @@
+//Addition of 2 numbers
+#include<stdio.h>
+
+int main()
+{
+    int No1 = 10;
+    int No2 = 11;
+    int Ans = 0;
+
+    Ans = No1 + No2;
+
+    printf("%d\n",Ans);
+
+    return 0;
+}
